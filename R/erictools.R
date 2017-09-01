5d7d8f15fa7a4dc4139f25e1bf9cb735e5e589dc
Eric Littmann <ericlittmann@MSKI1224.local>
2017-09-01 10:22:16 -0400

    erictools package info and namespace file...?

---
+++ R/erictools.R
@@ -0,0 +1,34 @@
+#' Eric's tools
+#'
+#' Eric's personal function library for data analysis
+#'
+#' To install/update this package, run: \code{devtools::install_github("elittmann/erictools")}
+#'
+#' @author
+#' Eric Littmann
+#'
+#' Maintainer: Eric Littmann
+#' @name erictools
+#' @docType package
+NULL
+
+
+## git remote add origin git@github.com:elittmann/erictools.git
+## git push -u origin master
+#" ...Title...
+#"
+#" ...Description...
+#"
+#" @usage ...usage.code...
+#"
+#" ...details...
+#"
+#" @param .param1. ...param1.description...
+#" @param .param2. ...param2.description...
+#" @return ...description.of.data.returned...
+#" @examples
+#" ...examples.here....
+#" @keywords keyword1 keyword2 ...
+#" @seealso \code{\link{cdiff.method}}
+#" @author Eric Littmann
+#" @export
