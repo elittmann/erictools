f34200ccd7ec381f5ac18138ad379f9cbcfde7f1
Eric Littmann <ericlittmann@MSKI1224.local>
2017-09-22 12:05:43 -0400

    removed title by 16S pool

--- R/plot_yt_tax.R
+++ R/plot_yt_tax.R
@@ -1,5 +1,5 @@
 #'General 16S Abundance Plot
-#'This function reads uparse output files from the specified directory and generates a 16S abundance plot at the species level
+#' @details This function reads uparse output files from the specified directory and generates a 16S abundance plot at the species level
 #' @param phylo phyloseq object (can use read_uparse to get uparse outputs into phyloseq...)
 #' @param pctseqs When TRUE will return plots with % abundance, when false will return plots using number of sequences not normalized to 1
 #' @param threshold Minimum number of sequences (numseqs) or % abundance (pctseqs) required for labeling taxons at species level
@@ -29,8 +29,7 @@ plot_yt_tax <- function(phylo,pctseqs=T,threshold=.1,taxtextsize=3,labeltextsize
     scale_fill_manual(values=pal) +
     geom_text(aes(x=sample,y=y.text,label=tax.label),lineheight=.6,size=taxtextsize) +
     ylab("Number of Sequences") +
-    xlab("") +
-    ggtitle(paste(pools))
+    xlab("")
 
   return(gg)
   }else{
@@ -51,8 +50,7 @@ plot_yt_tax <- function(phylo,pctseqs=T,threshold=.1,taxtextsize=3,labeltextsize
       scale_fill_manual(values=pal) +
       geom_text(aes(x=sample,y=y.text,label=tax.label),lineheight=.6,size=taxtextsize) +
       ylab("Number of Sequences") +
-      xlab("") +
-      ggtitle(paste(pools))
+      xlab("")
 
     return(gg)
 
