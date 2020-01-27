4effebc2336d521cebb80adce4c4a078ba255f00
Eric Littmann <eric.littmann@gmail.com>
2020-01-27 14:02:34 -0600

    Update extract_legend.R

--- R/extract_legend.R
+++ R/extract_legend.R
@@ -1,8 +1,8 @@
 #' Extract Legend Function
 #' This function extracts a legend from a ggplot object, returning a ggplot object
 #' @export
-extract_legend<-function(a.gplot){
-  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+extract_legend<-function(ggplot_object){
+  tmp <- ggplot_gtable(ggplot_build(ggplot_object))
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   legend
